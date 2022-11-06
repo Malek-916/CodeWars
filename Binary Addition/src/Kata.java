6692a4e516ea811c02cf3bb64423d6603e2bb8bd
JavaDev <116810620+Malek-916@users.noreply.github.com>
2022-11-06 22:55:48 +0100

    Add files via upload

---
+++ Binary Addition/src/Kata.java
@@ -0,0 +1,31 @@
+public class Kata {
+    public static void main(String[] args) {
+        System.out.println(binaryAddition(5,9));
+    }
+    public static String binaryAddition(int a, int b){
+
+        String bin = "";
+        int resto =0;
+        int num = a+b;
+
+        do {
+            resto =  num % 2;
+            System.out.println(num);
+            if (resto==0)
+                bin += "0";
+            else
+                bin +=1;
+            num /= 2;
+        }while (num != 0);
+        return new StringBuilder(bin).reverse().toString();
+    }
+
+    public static String reverseString(String str){
+        char ch[]=str.toCharArray();
+        String rev="";
+        for(int i=ch.length-1;i>=0;i--){
+            rev+=ch[i];
+        }
+        return rev;
+    }
+}
