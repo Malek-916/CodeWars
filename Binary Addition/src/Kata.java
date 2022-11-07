d935a59b7bcbb6368fb3347ef97bfcba42af87be
JavaDev <116810620+Malek-916@users.noreply.github.com>
2022-11-07 09:02:32 +0100

    Update Kata.java

--- Binary Addition/src/Kata.java
+++ Binary Addition/src/Kata.java
@@ -20,12 +20,5 @@ public class Kata {
         return new StringBuilder(bin).reverse().toString();
     }
 
-    public static String reverseString(String str){
-        char ch[]=str.toCharArray();
-        String rev="";
-        for(int i=ch.length-1;i>=0;i--){
-            rev+=ch[i];
-        }
-        return rev;
-    }
+    
 }
